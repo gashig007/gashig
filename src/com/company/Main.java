b6aaee2fea12b7e2fe82c5b2c9d84b8817cea435
gashig007 <rustamgashig@gmail.com>
2021-12-06 22:11:23 +0600

    set commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
-public class Main {
+import java.util.Random;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(generateRandomAge());
+
+    }
+    public static int generateRandomAge() {
+        Random random = new Random();
+        int math = random.nextInt(50);
+        return  math;
     }
 }
